f6583cb1a1b0be1d128b72bbb55b83aeb2b3f869
Martin Polder <martinpolder@gmail.com>
2019-07-10 13:58:17 -0400

    starting over lol

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,64 +1,57 @@
-// start with an opening screen with a click event to start the Game
-$("#start").click(runQuiz);
+// Variables & html ref
 
-
-// showing the quiz question in the interval
-var showQ;
-
-var countdown = 30;
-
-// counters for correct, incorrect and incomplete answers
-var correct = 0;
-var incorrect = 0;
-var unAns = 0;
-
-// have an array with all the questions and answers run through a for loop
-
-// question array
 var qBank = [
-   
-//    question 1
     {
-        question: "Chicken cross road?",
-        answers: {
-            a: "Who is this",
-            b: "Hmmmm",
-            c: "No.",
-            d: "Maybe",
-        },
-        correctAns: "c"
+        question = "What is your favorite color?",
+        a = "Firetruck",
+        b = "Velcro",
+        c = "Pants",
+        d = "Green",
+        correctAns = "d",
     },
 
-    // question 2
     {
-        question: "Why charlie hate?",
-        answers: {
-            a: "mean man",
-            b: "bastard man",
-            c: "dumb man",
-            d: "loud man",
-        },
-        correctAns: "b"
+        question = "Why are you sad?",
+        a = "No money",
+        b = "Plane crash",
+        c = "Dog died",
+        d = "Flat feet",
+        correctAns = "a",
+
+    },
+
+    {
+        question = "Stop tickling me",
+        a = "Okay",
+        b = "I'm not",
+        c = "You're just itchy",
+        d = "I'm almost done",
+        correctAns = "b",
+    },
+
+    {
+        question = "I'm out of ideas",
+        a = "Me too",
+        b = "Think harder",
+        c = "I'm tired",
+        d = "I'm hungry",
+        correctAns = "d",
     },
 
 ]
 
-// run a for loop for the length of the array to choose a question
-
-// insert that selected question into the question div and make it appear for 30 seconds before moving onto the next question
 
 
 
-
-// each answer is a clickable button that triggers the next screen
-
-
-// if user answer is correct, inform them for 5sec before auto loading the next question, store as correct answer
-
-// if user answer is incorrect, inform them, and show right answer for 5sec and auto load next question, store as incorrect answer
-
-// if user runs out of time, display an out of time message, show them the correct answer and load the next question
-//         store as an unanswered question
+lastQ = questions.length-1;
+currentQ = 0;
 
 
-// when loop finishes, display the correct, incorrect, and unanswered values and a button to start the game over
+function askQ() {
+    let q = questions[currentQ];
+    question.html("<p>" + q.question + "</p>")
+    a.html(q.a);
+    b.html(q.b);
+    c.html(q.c);
+    d.html(q.d);
+}
